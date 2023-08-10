0d49b31e340093e5ae41ec5bf57d2e16f8931470
Janaína Gomes <jana.gomes90@hotmail.com>
2023-08-10 17:10:27 -0300

    adicinando

---
+++ ExerciciosCod3r/src/Fundamentos/operadores/Logicos.java
@@ -0,0 +1,45 @@
+package Fundamentos.operadores;
+
+public class Logicos {
+
+	public static void main(String[] args) {
+		
+		boolean condicao1 = true;
+		boolean condicao2 = 3 > 7;
+		
+		System.out.println(condicao1 && condicao2);
+		System.out.println(condicao1 || condicao2);
+		System.out.println(condicao1 ^ condicao2);
+		System.out.println(!condicao1);
+		System.out.println(!!condicao1);
+		System.out.println(!condicao2);
+		
+		System.out.println("\nTabela Verdade E (and) &&");
+		System.out.println(true && true);
+		System.out.println(true && false);
+//		System.out.println(false && true);
+//		System.out.println(false && false);
+
+		System.out.println("\nTabela Verdade OU (or) ||");
+//		System.out.println(true || true);
+//		System.out.println(true || false);
+		System.out.println(false || true);
+		System.out.println(false || false);
+
+		System.out.println("\nTabela Verdade OU EXCLUSIVO (xor) ^");
+		System.out.println(true ^ true);
+		System.out.println(true ^ false);
+		System.out.println(false ^ true);
+		System.out.println(false ^ false);
+
+		System.out.println("\nTabela Verdade Negação (not) !");
+		System.out.println(!true);
+		System.out.println(!false);
+		
+		
+		
+		
+		
+	}
+	
+}
