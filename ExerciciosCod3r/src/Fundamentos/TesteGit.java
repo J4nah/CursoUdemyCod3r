9c2abbaa5896ddb24aaeb63b071c0aa25ea391c6
Janaína Gomes <jana.gomes90@hotmail.com>
2023-08-10 15:58:21 -0300

    Update TesteGit.java

--- ExerciciosCod3r/src/Fundamentos/TesteGit.java
+++ ExerciciosCod3r/src/Fundamentos/TesteGit.java
@@ -4,8 +4,9 @@ public class TesteGit {
 
 	public static void main(String[] args) {
 		
-		System.out.println("OlÃ¡, mundo!!!!");
+		System.out.println("Olá¡, mundo!!!!");
 		System.out.println("Teste pelo Git");
+		System.out.println("mais uma alteração");
 	}
 	
 }
