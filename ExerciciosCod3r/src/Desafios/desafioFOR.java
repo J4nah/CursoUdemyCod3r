1d28ec76143441da11b5e591979d3c541150444e
Janaina Gomes <jana.gomes90@hotmail.com>
2023-09-04 17:31:03 -0300

    up

---
+++ ExerciciosCod3r/src/Desafios/desafioFOR.java
@@ -0,0 +1,13 @@
+package Desafios;
+
+public class desafioFOR {
+
+	public static void main(String[] args) {
+
+		for (String v = "#"; !v.equals("######"); v += "#") {
+			System.out.println(v);
+		}
+
+	}
+
+}
