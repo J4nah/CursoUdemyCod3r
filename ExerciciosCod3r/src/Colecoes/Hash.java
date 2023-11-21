8d18048ae4433334ae9de1a1fcf9395a228e49ef
Janaina Gomes <jana.gomes90@hotmail.com>
2023-11-20 23:47:57 -0300

    up

--- ExerciciosCod3r/src/Colecoes/Hash.java
+++ ExerciciosCod3r/src/Colecoes/Hash.java
@@ -3,17 +3,17 @@ package Colecoes;
 import java.util.HashSet;
 
 public class Hash {
-	
+
 	public static void main(String[] args) {
-		
+
 		HashSet<Usuario> usuario = new HashSet<Usuario>();
-		
+
 		usuario.add(new Usuario("José"));
 		usuario.add(new Usuario("Pedro"));
 		usuario.add(new Usuario("Astolpho"));
-		
+
 		System.out.println(usuario.contains(new Usuario("Astolpho")));
-		
+
 	}
 
 }
